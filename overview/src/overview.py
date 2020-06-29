b0fcdf30d5f61b8f577d74d4e77d033a464499bf
Nickdubbel <nick.dubbeldam@live.nl>
2020-06-29 17:16:21 +0200

    second try

--- overview/src/overview.py
+++ overview/src/overview.py
@@ -7,19 +7,48 @@ from rospy_tutorials.msg import Floats
 
 from nav_msgs.msg import Odometry
 
+thisdict = {
+    "RobotName"         :   "Toiletcleaner",
+    "Robot id"          :   1,
+    "Toilets cleaned"   :   5
+}
 
+global odom
+
+def printDictonary():
+    for x, y in thisdict.items():
+        print(x + " = " + y)     
+
+def Update(name,value):
+    thisdict[name] = value
+    
+def Remove(name):
+    thisdict.pop(name)
+
+def Clear():
+    thisdict.clear()
+
+def AddValue(name):
+    thisdict[name] = thisdict[name]+1
+
+def Getvalue(name):
+    return thisdict[name]
 
 
 def callback(msg):
-    print (msg.pose.pose)
+    odom = msg
+    printDictonary()
+    print(odom)
 
 
 if __name__ == '__main__':
+
 
     try:
         rospy.init_node('overview_py')
         odom_sub = rospy.Subscriber('/odom', Odometry, callback)
         rospy.spin()
+        
         
             
     except rospy.ROSInterruptException:
