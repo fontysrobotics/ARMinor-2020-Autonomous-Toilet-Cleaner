e08b8288cad52280fd2db0341b916570ac4bd959
Nickdubbel <nick.dubbeldam@live.nl>
2020-06-29 17:11:53 +0200

    Revert "Combined the 2 projects"
    This reverts commit 1c0c10456cf6964b4a04b61d8f1817988f9e738e.
    
    	modified:   README.md

--- overview/src/overview.py
+++ overview/src/overview.py
@@ -7,48 +7,19 @@ from rospy_tutorials.msg import Floats
 
 from nav_msgs.msg import Odometry
 
-thisdict = {
-    "RobotName"         :   "Toiletcleaner",
-    "Robot id"          :   1,
-    "Toilets cleaned"   :   5
-}
 
-global odom
-
-def printDictonary():
-    for x, y in thisdict.items():
-        print(x + " = " + y)     
-
-def Update(name,value):
-    thisdict[name] = value
-    
-def Remove(name):
-    thisdict.pop(name)
-
-def Clear():
-    thisdict.clear()
-
-def AddValue(name):
-    thisdict[name] = thisdict[name]+1
-
-def Getvalue(name):
-    return thisdict[name]
 
 
 def callback(msg):
-    odom = msg
-    printDictonary()
-    print(odom)
+    print (msg.pose.pose)
 
 
 if __name__ == '__main__':
-
 
     try:
         rospy.init_node('overview_py')
         odom_sub = rospy.Subscriber('/odom', Odometry, callback)
         rospy.spin()
-        
         
             
     except rospy.ROSInterruptException:
--- tcmoveit/src/move_arm.cpp
+++ tcmoveit/src/move_arm.cpp
@@ -80,7 +80,7 @@ int main(int argc, char** argv)
   // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
   // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
   namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("shoulder");
+  moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
   visual_tools.deleteAllMarkers();
 
   // Remote control is an introspection tool that allows users to step through a high level script
@@ -180,6 +180,64 @@ int main(int argc, char** argv)
   visual_tools.trigger();
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
 
+  // Planning with Path Constraints
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  //
+  // Path constraints can easily be specified for a link on the robot.
+  // Let's specify a path constraint and a pose goal for our group.
+  // First define the path constraint.
+  moveit_msgs::OrientationConstraint ocm;
+  ocm.link_name = "panda_link7";
+  ocm.header.frame_id = "panda_link0";
+  ocm.orientation.w = 1.0;
+  ocm.absolute_x_axis_tolerance = 0.1;
+  ocm.absolute_y_axis_tolerance = 0.1;
+  ocm.absolute_z_axis_tolerance = 0.1;
+  ocm.weight = 1.0;
+
+  // Now, set it as the path constraint for the group.
+  moveit_msgs::Constraints test_constraints;
+  test_constraints.orientation_constraints.push_back(ocm);
+  move_group.setPathConstraints(test_constraints);
+
+  // We will reuse the old goal that we had and plan to it.
+  // Note that this will only work if the current state already
+  // satisfies the path constraints. So, we need to set the start
+  // state to a new pose.
+  robot_state::RobotState start_state(*move_group.getCurrentState());
+  geometry_msgs::Pose start_pose2;
+  start_pose2.orientation.w = 1.0;
+  start_pose2.position.x = 0.55;
+  start_pose2.position.y = -0.05;
+  start_pose2.position.z = 0.8;
+  start_state.setFromIK(joint_model_group, start_pose2);
+  move_group.setStartState(start_state);
+
+  // Now we will plan to the earlier pose target from the new
+  // start state that we have just created.
+  move_group.setPoseTarget(target_pose1);
+
+  // Planning with constraints can be slow because every sample must call an inverse kinematics solver.
+  // Lets increase the planning time from the default 5 seconds to be sure the planner has enough time to succeed.
+  move_group.setPlanningTime(10.0);
+
+  success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  ROS_INFO_NAMED("tutorial", "Visualizing plan 3 (constraints) %s", success ? "" : "FAILED");
+
+  // Visualize the plan in RViz
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishAxisLabeled(start_pose2, "start");
+  visual_tools.publishAxisLabeled(target_pose1, "goal");
+  visual_tools.publishText(text_pose, "Constrained Goal", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+  visual_tools.trigger();
+  visual_tools.prompt("next step");
+
+  // When done with the path constraint be sure to clear it.
+  move_group.clearPathConstraints();
+
+  // Since we set the start state we have to clear it before planning other paths
+  move_group.setStartStateToCurrentState();
 
   // Cartesian Paths
   // ^^^^^^^^^^^^^^^
